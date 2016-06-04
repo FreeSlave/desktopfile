0ad6f0fc471ef105bfd9357b47f9117cc92efc47
FreeSlave <freeslave93@gmail.com>
2016-06-05 00:11:25 +0300

    Try to remove coverage statistics files of dependencies before running doveralls

--- travis-script.sh
+++ travis-script.sh
@@ -6,6 +6,7 @@ if [ "$USE_DOVERALLS" = "true" ]; then
     wget -O doveralls "https://github.com/ColdenCullen/doveralls/releases/download/v1.2.0/doveralls_linux_travis"
     chmod +x doveralls
     dub test -b unittest-cov --compiler=${DC}
+    rm ..-*
     ./doveralls
 else
     dub test --compiler=${DC}
